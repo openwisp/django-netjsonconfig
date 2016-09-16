28c10b2cc4d32c4cdee2954bab10c6d36ecb7c32
Federico Capoano <nemesis@ninux.org>
2016-09-16 18:40:54 +0200

    [vpn] Added default values for OpenVPN ca, cert and key

--- django_netjsonconfig/vpn_backends.py
+++ django_netjsonconfig/vpn_backends.py
@@ -14,6 +14,28 @@ limited_schema['properties']['openvpn']['items'].update({
     "oneOf": [{"$ref": "#/definitions/server"}]
 })
 
+# default values for ca, cert and key
+limited_schema['definitions']['tunnel']['properties']['ca']['default'] = 'ca.pem'
+limited_schema['definitions']['tunnel']['properties']['cert']['default'] = 'cert.pem'
+limited_schema['definitions']['tunnel']['properties']['key']['default'] = 'key.pem'
+limited_schema['properties']['files']['default'] = [
+    {
+        "path": "ca.pem",
+        "mode": "0644",
+        "contents": "{{ ca }}"
+    },
+    {
+        "path": "cert.pem",
+        "mode": "0644",
+        "contents": "{{ cert }}"
+    },
+    {
+        "path": "key.pem",
+        "mode": "0644",
+        "contents": "{{ key }}"
+    }
+]
+
 
 class OpenVpn(BaseOpenVpn):
     schema = limited_schema
