0feb06b566417760eb8c81146e0340e1973c0f0f
Federico Capoano <nemesis@ninux.org>
2017-01-11 11:30:50 +0100

    [appconfig] Load models in dedicated __setmodels__ method
    This changes makes it possible to reuse this appconfig
    in order to avoid duplicating signal connection logic

--- django_netjsonconfig/apps.py
+++ django_netjsonconfig/apps.py
@@ -12,21 +12,28 @@ class DjangoNetjsonconfigApp(AppConfig):
     label = 'django_netjsonconfig'
     verbose_name = _('Configurations')
 
+    def __setmodels__(self):
+        """
+        This method allows third party apps to set their own custom models
+        """
+        from .models import Config, VpnClient
+        self.config_model = Config
+        self.vpnclient_model = VpnClient
+
     def connect_signals(self):
         """
         * m2m validation before templates are added/removed to a config
         * automatic vpn client management on m2m_changed
         * automatic vpn client removal
         """
-        from .models import Config, VpnClient
-        m2m_changed.connect(Config.clean_templates,
-                            sender=Config.templates.through)
-        m2m_changed.connect(Config.templates_changed,
-                            sender=Config.templates.through)
-        m2m_changed.connect(Config.manage_vpn_clients,
-                            sender=Config.templates.through)
-        post_delete.connect(VpnClient.post_delete,
-                            sender=VpnClient)
+        m2m_changed.connect(self.config_model.clean_templates,
+                            sender=self.config_model.templates.through)
+        m2m_changed.connect(self.config_model.templates_changed,
+                            sender=self.config_model.templates.through)
+        m2m_changed.connect(self.config_model.manage_vpn_clients,
+                            sender=self.config_model.templates.through)
+        post_delete.connect(self.vpnclient_model.post_delete,
+                            sender=self.vpnclient_model)
 
     def check_settings(self):
         if settings.DEBUG is False and REGISTRATION_ENABLED and not SHARED_SECRET:  # pragma: nocover
@@ -35,5 +42,6 @@ class DjangoNetjsonconfigApp(AppConfig):
                                        'NETJSONCONFIG_REGISTRATION_ENABLED to False')
 
     def ready(self):
+        self.__setmodels__()
         self.check_settings()
         self.connect_signals()
